5606192a8ed427f83d295d1fcc4ded6503422c42
Tina Garg <200260055@iitb.ac.in>
2022-06-15 00:27:32 +0530

    added user authentication

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 const UserRoutes = require('./routes/userroutes');
 const BlogRoutes = require('./routes/blogroutes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+//import routes
+const authRoutes = require('./routes/auth');
+const { db } = require('./models/User');
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/bloggerhead", {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
 }).then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
@@ -18,12 +27,17 @@ app.get('/', (req,res) => {
     res.json({"message": "Server is running"});
 });
 
-
+// middlewares
 app.use(express.json());
 app.use('/api',UserRoutes);
 app.use('/api',BlogRoutes);
+app.use(bodyParser.json());
+app.use(cors());
 
+//routes middleware
+app.use('/api', authRoutes);
 
-app.listen(2000 , () => {                     //add port
-    console.log("Server is running ");
+//add port
+app.listen(2000 , () => {                     
+    console.log("Server is running on port 2000");
 });
